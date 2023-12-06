9879a0eb3195bfae175bc9b236596ce06615adbe
phillipesimon <phillipe.simon@gmail.com>
2023-12-06 20:37:13 -0300

    Resumo das transações

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -21,6 +21,14 @@ export async function transactionsRoutes(app: FastifyInstance) {
     return { transactions }
   })
 
+  app.get('/summary', async () => {
+    const summary = await knex('transactions')
+      .sum('amount', { as: 'amount' })
+      .first()
+
+    return { summary }
+  })
+
   app.post('/', async (request, replay) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
