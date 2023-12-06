a68b223bccb2ee916e5bdde7581276efca2175a1
phillipesimon <phillipe.simon@gmail.com>
2023-12-06 20:36:32 -0300

    Listagem das transações

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -4,6 +4,23 @@ import { z } from 'zod'
 import { randomUUID } from 'crypto'
 
 export async function transactionsRoutes(app: FastifyInstance) {
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select()
+    return { transactions }
+  })
+
+  app.get('/:id', async (request) => {
+    const getTransectionParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = getTransectionParamsSchema.parse(request.params)
+
+    const transactions = await knex('transactions').where('id', id).first()
+
+    return { transactions }
+  })
+
   app.post('/', async (request, replay) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
